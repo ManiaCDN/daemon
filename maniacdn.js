15d2ebb6df7818d87168341fb14d7d9886248fe4
Tom Valk <tomvalk@gmail.com>
2015-12-01 22:23:30 +0100

    Cleanup package.json and reworked the starter file.

--- maniacdn.js
+++ maniacdn.js
@@ -32,30 +32,8 @@ async.series([
         database.makeConnection(function (err, connection) {
             console.log("Database connection made!");
 
-            var webserver = require('./webserver');
-            var check = require('./check');
-
             var debug = require('debug')('ManiaCDN:server');
-            var http = require('http');
-
-            /**
-             * Get port from environment and store in Express.
-             */
-            port = normalizePort(config.webserver.port);
-            webserver.set('port', port);
-
-            /**
-             * Create HTTP server.
-             */
-            server = http.createServer(webserver);
-
-            /**
-             * Listen on provided port, on all network interfaces.
-             */
-            server.listen(port);
-            server.on('error', onError);
-            server.on('listening', onListening);
-
+            var check = require('./check');
 
             return callback(null);
         });
@@ -66,66 +44,3 @@ async.series([
         process.exit(1);
     }
 });
-
-
-
-
-
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort(val) {
-    var port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    //debug('Listening on ' + bind);
-    console.log('Listening on ' + bind);
-    console.log('');
-}
